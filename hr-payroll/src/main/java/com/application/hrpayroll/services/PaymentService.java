c7db0d7f93d8ab68609b24e7e28e8dbd0b0fab4d
Messias Zaleuski <messias.zaleuski@gmail.com>
2021-05-13 11:53:19 -0300

    Feign client

--- hr-payroll/src/main/java/com/application/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/application/hrpayroll/services/PaymentService.java
@@ -1,14 +1,22 @@
 package com.application.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.application.hrpayroll.entities.Payment;
+import com.application.hrpayroll.entities.Worker;
+import com.application.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
-	public Payment getPayments(long workerId, int days) {
-		return new Payment("Bob", 200.0, days);
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
+	public Payment getPayment(long workerId, int days) {
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
 }
--- hr-worker/src/main/java/com/application/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/application/hrworker/resources/WorkerResource.java
@@ -1,14 +1,12 @@
 package com.application.hrworker.resources;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.application.hrworker.entities.Worker;
 import com.application.hrworker.repositories.WorkerRepository;
 
